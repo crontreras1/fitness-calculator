aaabbafbeb2391f2e9961186fc4921c1c3d5f189
cristian contreras <cr.ontreras@gmail.com>
2023-03-25 16:14:46 -0500

    creación del archivo js y las constantes de peso altura y edad para recibir su  valor

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Obteber valor del peso
+const weight = document.getElementById('input-weight')
+const weightValue = weight.value
+
+// Obteber valor de la altura
+const height = document.getElementById('input-height')
+const heightValue = height.value
+
+// Obteber valor de la edad
+const age = document.getElementById('input-age')
+const ageValue = age.value
